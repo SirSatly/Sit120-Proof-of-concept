78e5adf6f127dd5dd7a12b1f308f6aa5b4b53d20
SirSatly <85006680+SirSatly@users.noreply.github.com>
2022-08-05 19:46:54 +1000

    Add everything

---
+++ script.js
@@ -0,0 +1,7 @@
+function activateOverlay() {
+    document.getElementById("overlay").style.display = "block";
+  }
+  
+  function deactivateOverlay() {
+    document.getElementById("overlay").style.display = "none";
+  }
